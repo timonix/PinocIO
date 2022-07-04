2c8d3cca077dd09ff0556cf7e93d58a27c053e1c
timjan <tjadeglans@gmail.com>
2022-07-04 21:46:45 +0200

    .

---
+++ template_AI/network_collection.py
@@ -0,0 +1,76 @@
+import torch
+from torch import nn
+
+
+
+import torch.nn.functional as F
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.in2hidden = nn.Linear(input_size + hidden_size, hidden_size)
+        self.in2output = nn.Linear(input_size + hidden_size, output_size)
+
+    def forward(self, x, hidden_state):
+        combined = torch.cat((x, hidden_state), 1)
+        hidden = torch.sigmoid(self.in2hidden(combined))
+        output = self.in2output(combined)
+        return output, hidden
+
+    def init_hidden(self):
+        return nn.init.kaiming_uniform_(torch.empty(1, self.hidden_size))
+
+
+class Encoder(nn.Module):
+    def __init__(self, width, height, num_filters_layer_one, num_filters_layer_two, latent_size):
+        super(Encoder, self).__init__()
+        # Our first linear layer take input_size, in this case 784 nodes to 50
+        # and our second linear layer takes 50 to the num_classes we have, in
+        # this case 10.
+        self.fc1 = nn.Conv2d(3, num_filters_layer_one, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.fc2 = nn.Conv2d(num_filters_layer_one, num_filters_layer_two, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.fc_lin = nn.Linear(width * height * num_filters_layer_two / 4, latent_size)
+
+
+class Decoder(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(Decoder, self).__init__()
+
+        self.decoder = torch.nn.Sequential(
+            torch.nn.Linear(input_size, input_size*2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(input_size*2, input_size*4),
+            torch.nn.ReLU(),
+            torch.nn.Linear(input_size*4, input_size*8),
+            torch.nn.ReLU(),
+            torch.nn.Linear(input_size*8, output_size*4),
+            torch.nn.ReLU(),
+            torch.nn.Linear(output_size*4, output_size),
+            torch.nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class FC(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(FC, self).__init__()
+        # Our first linear layer take input_size, in this case 784 nodes to 50
+        # and our second linear layer takes 50 to the num_classes we have, in
+        # this case 10.
+        self.fc1 = nn.Linear(input_size, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        """
+        x here is the mnist images and we run it through fc1, fc2 that we created above.
+        we also add a ReLU activation function in between and for that (since it has no parameters)
+        I recommend using nn.functional (F)
+        """
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
---
+++ test/encoder_decoder_test.py
@@ -0,0 +1,77 @@
+import json
+
+import numpy as np
+import torch
+from PIL import Image
+from tqdm import tqdm
+
+from torch import optim  # For optimizers like SGD, Adam, etc.
+from torch import nn  # All neural network modules
+
+from template_AI.network_collection import Encoder
+from template_AI.network_collection import Decoder
+
+
+latent_size = 100
+height = 200
+width = 300
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 3
+num_chunks = 100
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+encoder = Encoder(width, height, 32, 32, latent_size).to(device)
+decoder = Decoder(latent_size, 300, height*width).to(device)
+
+if __name__ == "__main__":
+    # Load Training and Test data
+    train_dataset = []
+    facit_dict = dict()
+    with open("facit.json", "r") as facit:
+        facit_dict = json.load(facit)
+        for path, values in tqdm(facit_dict.items(), desc="Preparing train dataset"):
+            img = Image.open(path)
+            arr = np.asarray(img)
+            arr = np.moveaxis(arr, (2), (0))
+            train_dataset.append(arr)
+
+    train_dataset_total = np.stack(train_dataset)
+    chunks = np.array_split(train_dataset_total, num_chunks)
+
+    criterion = nn.MSELoss()
+    optimizer_en = optim.Adam(encoder.parameters(), lr=learning_rate)
+    optimizer_de = optim.Adam(decoder.parameters(), lr=learning_rate)
+
+    for epoch in range(num_epochs):
+        for i, chunk in enumerate(tqdm(chunks)):
+
+            # forward
+            latent_space = encoder(torch.FloatTensor(chunk))
+            output = decoder(latent_space)
+
+            loss = criterion(output, chunk)
+
+            # backward
+            optimizer_de.zero_grad()
+            optimizer_en.zero_grad()
+            loss.backward()
+
+            if (i + 1) % 5 == 0:
+                print(
+                    f"Epoch [{epoch + 1}/{num_epochs}], "
+                    f"Step [{i + 1}/{10000}], "
+                    f"Loss: {loss.item():.4f}"
+                )
+
+            # gradient descent or adam step
+            optimizer_en.step()
+            optimizer_de.step()
+
+            #if i > 1000:
+            #    break
+
+            # Moved this here, not tested
+        torch.save(encoder.state_dict(), "encoder.m")
+        torch.save(decoder.state_dict(), "decoder.m")
