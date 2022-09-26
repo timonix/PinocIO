5b6c7500255671f541c4982aff59b5ca07126d9e
timjan <tjadeglans@gmail.com>
2022-09-26 18:49:14 +0200

    ,

---
+++ test/auto_test_3.py
@@ -0,0 +1,129 @@
+import cv2
+import torch
+
+import numpy as np
+from PIL import Image
+
+import time
+
+from tqdm import tqdm
+from torch import optim  # For optimizers like SGD, Adam, etc.
+from torch import nn
+
+from template_AI.network_collection import Encoder_B as Encoder
+from template_AI.network_collection import Decoder_B as Decoder
+from image_generator import generate_train_dataset
+
+import glob
+
+from timeit import default_timer as timer
+
+
+
+#print(filelist)
+from cunker import cunker, decunker
+
+image_channels = 3
+encoder_base_size = 32
+decoder_base_size = 32
+latent_dim = 10
+
+learning_rate = 0.00001
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+encoder = Encoder(image_channels, encoder_base_size, latent_dim).to(device)
+decoder = Decoder(image_channels, decoder_base_size, latent_dim).to(device)
+
+criterion = nn.MSELoss()
+
+if __name__ == "__main__":
+
+    # x = np.array([np.array(Image.open(fname)) for fname in filelist])
+    # x = np.swapaxes(x, 1, 3)
+    # x = x[-1:]
+
+    # x = torch.FloatTensor(x)
+
+    # latent_space = encoder.forward(x)
+    # decoder(latent_space)
+
+    optimizer_en = optim.Adam(encoder.parameters(), lr=learning_rate)
+    optimizer_de = optim.Adam(decoder.parameters(), lr=learning_rate)
+
+    encoder.load_state_dict(torch.load("encoder.m"))
+    decoder.load_state_dict(torch.load("decoder.m"))
+
+    encoder.eval()
+    decoder.eval()
+
+    cam = cv2.VideoCapture(0)
+    for iteration in range(1000000):
+
+        ret_val, cv_img = cam.read()
+
+        color_coverted = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        pil_image = Image.fromarray(color_coverted)
+
+        img = pil_image
+
+        image_size = (640, 480)
+        cunk_size = 32
+
+        cc = cunker(cunk_size)
+        ll = []
+        for c in cc.cunk(img):
+            ll.append(np.array(c))
+        x = np.array(ll)
+
+        x = x/255
+
+
+        x = np.swapaxes(x, 1, 3)
+
+        x = torch.FloatTensor(x).to(device)
+
+
+        facit = x
+
+
+        for epoch in tqdm(range(1)):
+            # forward
+
+            latent_space = encoder(x)
+            output = decoder(latent_space)
+
+            #loss = criterion(output, facit)
+
+            #if epoch % 500 == 0:
+            #    print(
+            #        f"Loss: {loss.item():.8f}"
+            #    )
+
+                #torch.save(encoder.state_dict(), "encoder.m")
+                #torch.save(decoder.state_dict(), "decoder.m")
+
+            #optimizer_de.zero_grad()
+            #optimizer_en.zero_grad()
+            #loss.backward()
+
+            #optimizer_en.step()
+            #optimizer_de.step()
+
+
+
+
+
+        image = output.cpu().detach().numpy()
+        image = np.swapaxes(image, 1, 3)
+        image = image*255
+        image = image.astype("uint8")
+
+        print(image.shape)
+        pila = []
+        for im in image:
+            pila.append(Image.fromarray(im))
+
+        dc = decunker(image_size)
+        dc.decunk(pila).show()
+
---
+++ test/cunker.py
@@ -0,0 +1,91 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+
+def __ressample__(arr, N):
+    if len(arr[0][0]) % N != 0 or len(arr[0]) % N != 0:
+        print("cropping image")
+        print(str(len(arr[0][0])) + "x" + str(len(arr[0])) + " is not divisible by " + str(N) + "x" + str(N))
+
+    num_horizontal_cunks = int(len(arr[0][0]) / N)
+    num_vertical_cunks = int(len(arr[0]) / N)
+
+    ret = [[None for _ in range(num_horizontal_cunks)] for _ in range(num_vertical_cunks)]
+
+    for h in range(num_horizontal_cunks):
+        for v in range(num_vertical_cunks):
+            ret[h][v] = arr[:, N * v:N * (v + 1), N * h:N * (h + 1)]
+
+    return ret
+
+
+class cunker():
+    def __init__(self, cunk_size=32):
+        self.cunk_size = cunk_size
+
+    def cunk(self, im):
+        im_width, im_height = im.size
+        row_width = self.cunk_size
+        row_height = self.cunk_size
+
+        cols = int(im_height / self.cunk_size)
+        rows = int(im_width / self.cunk_size)
+        n = 0
+        cunked_images = []
+        for i in range(0, cols):
+            for j in range(0, rows):
+                box = (j * row_width, i * row_height, j * row_width +
+                       row_width, i * row_height + row_height)
+                outp = im.crop(box)
+                cunked_images.append(outp)
+        return cunked_images
+
+
+class decunker():
+    def __init__(self, image_size,cunk_size=32):
+        self.cunk_size = cunk_size
+        self.image_size = image_size
+
+    def decunk(self, cunked_image):
+
+        images_to_merge = cunked_image
+        image1 = images_to_merge[0]
+        cols = int(self.image_size[0]/self.cunk_size)
+        rows = int(self.image_size[1]/self.cunk_size)
+        new_width = self.cunk_size * cols
+        new_height = self.cunk_size *rows
+        new_image = Image.new(image1.mode, (new_width, new_height))
+        for i in range(0, rows):
+            for j in range(0, cols):
+                image = images_to_merge[i * cols + j]
+                new_image.paste(image, (j * image.size[0], i * image.size[1]))
+
+        return new_image
+
+
+def cunk_test():
+    img = Image.open("C:\\Users\\tjade\\PycharmProjects\\PinocIO\\Train Images\\Cunk_test.bmp")
+
+    cam = cv2.VideoCapture(0)
+    ret_val, cv_img = cam.read()
+
+    print(cv_img.shape)
+
+    color_coverted = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+    pil_image = Image.fromarray(color_coverted)
+
+    img = pil_image
+
+    image_size = (640, 480)
+    cunk_size = 32
+
+    cc = cunker(cunk_size)
+    dc = decunker(image_size)
+
+    ccc = cc.cunk(img)
+    print(len(ccc))
+    dc.decunk(ccc).show()
+
+
+#cunk_test()
