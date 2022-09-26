0dbd01c9be50400204c424b1c0c1d027cbe1e57a
Noc <t.gelotte@gmail.com>
2022-09-26 19:12:57 +0200

    Commit of working auto_test_2.py

--- test/auto_test_2.py
+++ test/auto_test_2.py
@@ -6,7 +6,6 @@ from PIL import Image
 import time
 
 from tqdm import tqdm
-from tqdm import trange
 from torch import optim  # For optimizers like SGD, Adam, etc.
 from torch import nn
 
@@ -15,12 +14,9 @@ from template_AI.network_collection import Decoder_B as Decoder
 from image_generator import generate_train_dataset
 
 import glob
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 
 
 filelist = glob.glob('../Train Images/*.png')
-test_files = glob.glob('../Test Images/')
 
 #print(filelist)
 
@@ -34,7 +30,6 @@ learning_rate = 0.00001
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 encoder = Encoder(image_channels, encoder_base_size, latent_dim).to(device)
-
 decoder = Decoder(image_channels, decoder_base_size, latent_dim).to(device)
 
 criterion = nn.MSELoss()
@@ -53,9 +48,7 @@ if __name__ == "__main__":
     optimizer_en = optim.Adam(encoder.parameters(), lr=learning_rate)
     optimizer_de = optim.Adam(decoder.parameters(), lr=learning_rate)
 
-    plt.rcParams["figure.figsize"] = [7.50, 3.50]
-    plt.rcParams["figure.autolayout"] = True
-
+    for iteration in range(10):
 
         generate_train_dataset(1000, 32, 32)
 
@@ -67,15 +60,9 @@ if __name__ == "__main__":
 
         x = torch.FloatTensor(x).to(device)
 
-        if epoch % 1 == 0:
-            #print(
-            #    # f"Epoch [{epoch + 1}/{100}], "
-            #    f"Loss: {loss.item():.4f}"
-            #)
-            torch.save(encoder.state_dict(), "encoder.m")
-            torch.save(decoder.state_dict(), "decoder.m")
 
         facit = x
+
 
         for epoch in tqdm(range(3000)):
             # forward
@@ -83,28 +70,31 @@ if __name__ == "__main__":
             latent_space = encoder(x)
             output = decoder(latent_space)
 
+            loss = criterion(output, facit)
 
-        torch.save(encoder.state_dict(), "encoder.m")
-        torch.save(decoder.state_dict(), "decoder.m")
+            if epoch % 500 == 0:
+                print(
+                    f"Loss: {loss.item():.8f}"
+                )
 
-        if epoch % 500 == 0:
-            print(
-                f"Loss: {loss.item():.8f}"
-            )
+                torch.save(encoder.state_dict(), "encoder.m")
+                torch.save(decoder.state_dict(), "decoder.m")
 
-            torch.save(encoder.state_dict(), "encoder.m")
-            torch.save(decoder.state_dict(), "decoder.m")
-
-
-    image = output[0] * 255
-    image = image.detach().numpy()
-    image = np.swapaxes(image, 0, 2)
-    image = image.astype(np.int8)
-    Image.fromarray(image, 'RGB').show()
-
+            optimizer_de.zero_grad()
+            optimizer_en.zero_grad()
+            loss.backward()
 
             optimizer_en.step()
             optimizer_de.step()
 
+            pass
 
-    pass
+    image = output[0]*255
+    image = image.cpu().detach().numpy()
+    for i in image.tolist():
+        print(i)
+    image = np.swapaxes(image, 0, 2)
+    image = image.astype("uint8")
+    image = image + 11
+
+    Image.fromarray(image, 'RGB').show()
