00867959d938a15b57b4cafbcb44b0f0a9963b17
timjan <tjadeglans@gmail.com>
2022-09-26 19:27:18 +0200

    added 64*64 enc dec

--- template_AI/network_collection.py
+++ template_AI/network_collection.py
@@ -21,13 +21,101 @@ class RNN(nn.Module):
         return nn.init.kaiming_uniform_(torch.empty(1, self.hidden_size))
 
 
+class Encoder64(nn.Module):
+    def __init__(self,
+                 num_input_channels: int,
+                 latent_dim: int,
+                 layer_params=None, # small to large
+                 act_fn: object = nn.GELU):
+        """
+        Inputs:
+            - num_input_channels : Number of input channels of the image. For CIFAR, this parameter is 3
+            - latent_dim : Dimensionality of latent representation z
+            - act_fn : Activation function used throughout the encoder network
+        """
+        super().__init__()
+        if layer_params is None:
+            layer_params = [32, 32, 32, 32, 32, 64, 64]
+        self.net = nn.Sequential(
+            nn.Conv2d(num_input_channels, layer_params[0], kernel_size=3, padding=1, stride=2),  # 64x64 => 32x32
+            act_fn(),
+            nn.Conv2d(layer_params[0], layer_params[1], kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(layer_params[1], layer_params[2], kernel_size=3, padding=1, stride=2),  # 32x32 => 16x16
+            act_fn(),
+            nn.Conv2d(layer_params[2], layer_params[3], kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(layer_params[3], layer_params[4], kernel_size=3, padding=1, stride=2),  # 16x16 => 8x8
+            act_fn(),
+            nn.Conv2d(layer_params[4], layer_params[5], kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(layer_params[5], layer_params[6], kernel_size=3, padding=1, stride=2),  # 8x8 => 4x4
+            act_fn(),
+            nn.Flatten(),  # Image grid to single feature vector
+            nn.Linear(16 * layer_params[6], latent_dim)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Decoder64(nn.Module):
+
+    def __init__(self,
+                 num_input_channels: int,
+                 latent_dim: int,
+                 layer_params=None,
+                 act_fn: object = nn.GELU):
+        """
+        Inputs:
+            - num_input_channels : Number of channels of the image to reconstruct. For CIFAR, this parameter is 3
+            - base_channel_size : Number of channels we use in the last convolutional layers. Early layers might use a duplicate of it.
+            - latent_dim : Dimensionality of latent representation z
+            - act_fn : Activation function used throughout the decoder network
+        """
+        super().__init__()
+        if layer_params is None:
+            layer_params = [64, 64, 32, 32, 32, 32, 32, 32]
+
+        self.linear = nn.Sequential(
+            nn.Linear(latent_dim, 16 * layer_params[0]),
+            act_fn()
+        )
+        self.net = nn.Sequential(  # large to small
+            nn.ConvTranspose2d(layer_params[0], layer_params[1], kernel_size=3, output_padding=1, padding=1, stride=2),
+            # 4x4 => 8x8
+            act_fn(),
+            nn.Conv2d(layer_params[1], layer_params[2], kernel_size=3, padding=1),
+            act_fn(),
+            nn.ConvTranspose2d(layer_params[2], layer_params[3], kernel_size=3, output_padding=1, padding=1, stride=2),
+            # 8x8 => 16x16
+            act_fn(),
+            nn.Conv2d(layer_params[3], layer_params[4], kernel_size=3, padding=1),
+            act_fn(),
+            nn.ConvTranspose2d(layer_params[4], layer_params[5], kernel_size=3, output_padding=1, padding=1, stride=2),
+            # 16x16 => 32x32
+            act_fn(),
+            nn.Conv2d(layer_params[5], layer_params[6], kernel_size=3, padding=1),
+            act_fn(),
+            nn.ConvTranspose2d(layer_params[7], num_input_channels, kernel_size=3, output_padding=1, padding=1, stride=2),
+
+            nn.Tanh()  # The input images is scaled between -1 and 1, hence the output has to be bounded as well
+        )
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = x.reshape(x.shape[0], -1, 4, 4)
+        x = self.net(x)
+        return x
+
+
 class Encoder_B(nn.Module):
 
     def __init__(self,
-                 num_input_channels : int,
-                 base_channel_size : int,
-                 latent_dim : int,
-                 act_fn : object = nn.GELU):
+                 num_input_channels: int,
+                 base_channel_size: int,
+                 latent_dim: int,
+                 act_fn: object = nn.GELU):
         """
         Inputs:
             - num_input_channels : Number of input channels of the image. For CIFAR, this parameter is 3
@@ -38,18 +126,18 @@ class Encoder_B(nn.Module):
         super().__init__()
         c_hid = base_channel_size
         self.net = nn.Sequential(
-            nn.Conv2d(num_input_channels, c_hid, kernel_size=3, padding=1, stride=2), # 32x32 => 16x16
+            nn.Conv2d(num_input_channels, c_hid, kernel_size=3, padding=1, stride=2),  # 32x32 => 16x16
             act_fn(),
             nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
             act_fn(),
-            nn.Conv2d(c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 16x16 => 8x8
+            nn.Conv2d(c_hid, 2 * c_hid, kernel_size=3, padding=1, stride=2),  # 16x16 => 8x8
             act_fn(),
-            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
+            nn.Conv2d(2 * c_hid, 2 * c_hid, kernel_size=3, padding=1),
             act_fn(),
-            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 8x8 => 4x4
+            nn.Conv2d(2 * c_hid, 2 * c_hid, kernel_size=3, padding=1, stride=2),  # 8x8 => 4x4
             act_fn(),
-            nn.Flatten(), # Image grid to single feature vector
-            nn.Linear(2*16*c_hid, latent_dim)
+            nn.Flatten(),  # Image grid to single feature vector
+            nn.Linear(2 * 16 * c_hid, latent_dim)
         )
 
     def forward(self, x):
