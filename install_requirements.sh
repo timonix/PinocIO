4cb70cc8189e4f4c2b2f8095451ba43017bc0764
Noc <t.gelotte@gmail.com>
2022-10-10 21:31:21 +0200

    Some webcam, some install req.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,10 +1,11 @@
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
-pip3 install numpy==1.21.3
+pip install numpy==1.21.3
 
 # OpenCV install guide: https://www.youtube.com/watch?v=QzVYnG-WaM4&ab_channel=SamWestbyTech
 # https://raspberrypi-guide.github.io/programming/install-opencv
 sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
+sudo apt install libgl1-mesa-glx
 #sudo apt install python3-opencv
 sudo apt-get install python-opencv
 
--- test/webcam_shenanigans.py
+++ test/webcam_shenanigans.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture(-1)
+
+cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
