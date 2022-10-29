4f968598919121f0e4fa617088e3985ecbb790bd
Noc <t.gelotte@gmail.com>
2022-10-29 14:05:58 +0200

    pi testing

--- Robot/stepper_test.py
+++ Robot/stepper_test.py
@@ -1,10 +1,14 @@
 from time import sleep
 import RPi.GPIO as GPIO
+from sshkeyboard import listen_keyboard
+import sshkeyboard
+
 
 en = 14
 step = 15
 dir = 18
 
+print("Starting setup")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(en, GPIO.OUT)
@@ -15,16 +19,43 @@ GPIO.output(en, GPIO.LOW)
 GPIO.output(step, GPIO.LOW)
 GPIO.output(dir, GPIO.LOW)
 
-try:
-    while True:
+
+def press(key):
+    if key == 's':
+        print("stepping")
         GPIO.output(step, GPIO.HIGH)
         sleep(0.1)
         GPIO.output(step, GPIO.LOW)
-        sleep(0.1)
 
-except KeyboardInterrupt:
-    GPIO.output(en, GPIO.LOW)
-    GPIO.output(step, GPIO.LOW)
-    GPIO.output(dir, GPIO.LOW)
-    GPIO.cleanup()
-    pass
+    if key == 'q':
+        sshkeyboard.stop_listening()  # Shutdown is here to make check_action have a chance to shut itself down
+
+
+print("Starting stepping")
+
+listen_keyboard(
+        on_press=press,
+        # on_release=release,
+        # until='q',              # Abort when q is pressed
+)
+
+print("Interrupted")
+GPIO.output(en, GPIO.LOW)
+GPIO.output(step, GPIO.LOW)
+GPIO.output(dir, GPIO.LOW)
+GPIO.cleanup()
+
+#try:
+#    while True:
+#        GPIO.output(step, GPIO.HIGH)
+#        sleep(0.1)
+#        GPIO.output(step, GPIO.LOW)
+#        sleep(0.1)
+
+#except KeyboardInterrupt:
+#    print("Interrupted")
+#    GPIO.output(en, GPIO.LOW)
+#    GPIO.output(step, GPIO.LOW)
+#    GPIO.output(dir, GPIO.LOW)
+#    GPIO.cleanup()
+#    pass
