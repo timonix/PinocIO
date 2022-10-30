e74bb1ee7b4635d0cb7ffb8d2cf0c5c9dddd9a51
Noc <t.gelotte@gmail.com>
2022-10-30 11:28:20 +0100

    pi testing

--- Robot/stepper_test.py
+++ Robot/stepper_test.py
@@ -23,11 +23,11 @@ GPIO.output(dir, GPIO.LOW)
 def press(key):
     if key == 's':
         print("stepping")
-        for i in range(200):
+        for i in range(1600):
             GPIO.output(step, GPIO.HIGH)
-            sleep(0.005)
+            sleep(0.001)
             GPIO.output(step, GPIO.LOW)
-            sleep(0.005)
+            sleep(0.001)
 
     if key == 'q':
         sshkeyboard.stop_listening()  # Shutdown is here to make check_action have a chance to shut itself down
