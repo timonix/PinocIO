7a90c01260bc2fdf1f25af34ae86a73982722ac1
timjan <tjadeglans@gmail.com>
2022-12-15 00:32:49 +0100

    traincore test

--- pokenet/settings.py
+++ pokenet/settings.py
@@ -4,5 +4,5 @@ MODEL_PATH = "C:\\Users\\tjade\\Documents\\pokedata\\models/"
 DATA_PATH = "C:\\Users\\tjade\\Documents\\pokedata\\data/"
 
 if torch.cuda.is_available():
-    MODEL_PATH_RUNPOD = "models/"
-    DATA_PATH_RUNPOD = "data/"
+    MODEL_PATH = "models/"
+    DATA_PATH = "data/"
