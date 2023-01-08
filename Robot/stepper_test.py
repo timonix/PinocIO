6ef086a990f5cc823502e6dde44096cf8d38e4a6
Noc <t.gelotte@gmail.com>
2023-01-08 21:52:22 +0100

    robot stepper stuff

--- Robot/stepper_test.py
+++ Robot/stepper_test.py
@@ -4,35 +4,46 @@ from sshkeyboard import listen_keyboard
 import sshkeyboard
 
 
-en = 25
-step = 23
-dir = 24
+#en = 25
+step1 = 23
+dir1 = 24
+
+step2 = 25
+dir2 = 8
 
 print("Starting setup")
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(en, GPIO.OUT)
-GPIO.setup(step, GPIO.OUT)
-GPIO.setup(dir, GPIO.OUT)
+#GPIO.setup(en1, GPIO.OUT)
+GPIO.setup(step1, GPIO.OUT)
+GPIO.setup(dir1, GPIO.OUT)
 
-GPIO.output(en, GPIO.LOW)
-GPIO.output(step, GPIO.LOW)
-GPIO.output(dir, GPIO.LOW)
+GPIO.setup(step2, GPIO.OUT)
+GPIO.setup(dir2, GPIO.OUT)
+
+#GPIO.output(en, GPIO.LOW)
+GPIO.output(step1, GPIO.LOW)
+GPIO.output(dir1, GPIO.LOW)
+
+GPIO.output(step2, GPIO.LOW)
+GPIO.output(dir2, GPIO.LOW)
 
 
 def press(key):
     if key == 's':
         print("stepping")
         for i in range(1600):
-            GPIO.output(step, GPIO.HIGH)
+            GPIO.output(step1, GPIO.HIGH)
+            GPIO.output(step2, GPIO.HIGH)
             sleep(0.01)
-            GPIO.output(step, GPIO.LOW)
+            GPIO.output(step1, GPIO.LOW)
+            GPIO.output(step2, GPIO.LOW)
             sleep(0.01)
 
-    if key == 'l':
-        GPIO.output(en, GPIO.LOW)
-    if key == 'h':
-        GPIO.output(en, GPIO.HIGH)
+#    if key == 'l':
+#        GPIO.output(en, GPIO.LOW)
+#    if key == 'h':
+#        GPIO.output(en, GPIO.HIGH)
 
     if key == 'q':
         sshkeyboard.stop_listening()  # Shutdown is here to make check_action have a chance to shut itself down
